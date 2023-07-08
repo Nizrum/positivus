227c5d84f1a64e13fd1ee1f849bd13bb8c952877
nizrum <sema2005@icloud.com>
2023-07-08 16:15:11 +0400

    Доработан функционал меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,13 @@
 const mobileMenuButton = document.querySelector(".header__mobile-menu-button");
 const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu__link");
 
 mobileMenuButton.addEventListener("click", () => {
     menu.classList.toggle("menu_opened");
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle("menu_opened");
+    });
 });
